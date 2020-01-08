c81d1414f04c5a23d8aa2c2ee4e994e85f0c6dc8
liuweibin <liuweibin@linklogis.com>
2020-01-08 16:12:09 +0800

    添加些许算法

---
+++ src/main/java/leetcode/BasicCase.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+/**
+ * Description
+ * Author liuweibin
+ * Date 2020/1/8 11:18
+ **/
+public class BasicCase {
+    public static void main(String[] args) {
+    }
+
+    /**
+     * 有序数组合并，归并排序最后一步
+     * 输入:
+     * nums1 = [1,2,3,0,0,0], m = 3
+     * nums2 = [2,5,6],       n = 3
+     * <p>
+     * 输出: [1,2,2,3,5,6]
+     */
+    public static void merge(int[] nums1, int m, int[] nums2, int n) {
+        int i = m-- + --n;
+        while (n >= 0) {
+            nums1[i--] = m >= 0 && nums1[m] > nums2[n] ? nums1[m--] : nums2[n--];
+        }
+    }
+}
