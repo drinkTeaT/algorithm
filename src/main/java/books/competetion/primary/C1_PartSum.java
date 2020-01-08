8580fc0bff3da0bf449569cb7931af5894509496
drinktea <null>
2020-01-08 23:27:07 +0800

    书籍目录

--- src/main/java/books/competetion/primary/C1_PartSum.java
+++ src/main/java/books/competetion/primary/C1_PartSum.java
@@ -1,5 +1,8 @@
 package books.competetion.primary;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Description 给定整数a1,a2...an 判断是否可以选出若干数，使他们和恰好为k
  * n[1,20] ai[-10^8,10^8] k[-10^8,10^8]
@@ -7,31 +10,52 @@ package books.competetion.primary;
  * Date 2020/1/8 17:17
  **/
 public class C1_PartSum {
+    static long[] a = new long[]{2, 7, 11, 15};
+    static int n = a.length;
+    static long k = 9l;
+
     public static void main(String[] args) {
-        int n = 4;
-        long[] a = new long[]{1, 2, 4, 7};
-        long k = 11l;
-        solve(n, a, k);
+//        solve();
+        System.out.println(twoSum());
     }
 
-    public static void solve(int n, long[] a, long k) {
-        if (dfs(a, n, k, 0, 0)) {
+
+    public static void solve() {
+        if (dfs(0, 0)) {
             System.out.println("Yes");
         } else {
             System.out.println("No");
         }
     }
 
-    static boolean dfs(long[] a, int n, long k, int i, long sum) {
+    static boolean dfs(int i, long sum) {
         if (i == n) {
             return sum == k;
         }
-        if (dfs(a, n, k, i + 1, sum)) {
+        if (dfs(i + 1, sum)) {
             return true;
         }
-        if (dfs(a, n, k, i + 1, sum + a[i])) {
+        if (dfs(i + 1, sum + a[i])) {
             return true;
         }
         return false;
     }
+
+    /**
+     * 随便取出2个数 看其和相加是否等于目标值
+     *
+     * @return
+     */
+    static int[] twoSum() {
+        Map<Long, Integer> map = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            long complement = k - a[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(a[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
 }
