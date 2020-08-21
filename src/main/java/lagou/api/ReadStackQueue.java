9e25d2b08cf0343a2e7c05d15ca204c77f779f21
EDZ <111>
2020-08-21 18:41:48 +0800

    新增queue的比较器思路

--- src/main/java/lagou/api/ReadStackQueue.java
+++ src/main/java/lagou/api/ReadStackQueue.java
@@ -1,5 +1,8 @@
 package lagou.api;
 
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 /**
  * 阅读栈和队列
  */
@@ -8,23 +11,30 @@ public class ReadStackQueue {
 }
 
 /**
- *  继承vector，栈是线程安全的。当栈为空时，pop会报异常
- *  push pop isEmpty peek
+ * 继承vector，栈是线程安全的。当栈为空时，pop会报异常
+ * push pop isEmpty peek
  */
-class ReadStack{}
+class ReadStack {
+}
 
 /**
- *  LinkedList为继承类，当队列为空时，poll的值为null
- *  offer poll peek 无empty
+ * LinkedList为继承类，当队列为空时，poll的值为null
+ * offer poll peek 无empty
  */
-class ReadQueue{}
+class ReadQueue {
+    public void test() {
+        Queue<Integer> queue = new PriorityQueue<>((x, y) -> x - y);
+        // 比较器可以理解为:x - y < 0.x表示前一个，y表示后一个。
+    }
+}
 
 /**
- *  LinkedList为继承类，当队列为空时，poll的值为null
- *    offer poll peek 无empty 方法后加first或last
+ * LinkedList为继承类，当队列为空时，poll的值为null
+ * offer poll peek 无empty 方法后加first或last
  */
-class ReadDeque{}
+class ReadDeque {
+}
 
 /**
- *  记忆，stack有empty因为无的时候pop会报错，queue则返回null
+ * 记忆，stack有empty因为无的时候pop会报错，queue则返回null
  */
